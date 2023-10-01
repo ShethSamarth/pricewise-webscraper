6b7bb7102589cdbfd2b624d0b45eab6634eb1bb8
Samarth Sheth <samarth.sheth.29@gmail.com>
2023-10-01 17:38:46 +0530

    final

---
+++ components/navbar.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link"
+import Image from "next/image"
+
+const navIcons = [
+  { src: "/assets/icons/search.svg", alt: "Search" },
+  { src: "/assets/icons/black-heart.svg", alt: "Heart" },
+  { src: "/assets/icons/user.svg", alt: "User" },
+]
+
+const Navbar = () => {
+  return (
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="PriceWise Logo"
+          />
+
+          <p className="nav-logo">
+            Price<span className="text-primary">Wise</span>
+          </p>
+        </Link>
+
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className="object-contain hover:cursor-pointer"
+            />
+          ))}
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
